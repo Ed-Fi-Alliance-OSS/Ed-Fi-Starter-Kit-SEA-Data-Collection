069cb2c9944c6a3f3953a44335482015707c13e8
Jim McKay <james.mckay@instructure.com>
2021-04-10 11:16:06 -0500

    removed unused installer scripts

--- packer/scripts/installers/Install-SqlServerModule.ps1
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Licensed to the Ed-Fi Alliance under one or more agreements.
-# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
-# See the LICENSE and NOTICES files in the project root for more information.
-
-Write-Output "Installing SQLServer Powershell Module"
-
-Install-Module -Name SqlServer -MinimumVersion "21.1.18068" -Scope AllUsers -Force -AllowClobber | Out-Null
