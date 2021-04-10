1a924216c728d065ef8481a55af929980cdd15f1
Jim McKay <james.mckay@instructure.com>
2021-04-10 14:39:58 -0500

    fixed tls on chocolatey install

--- packer/scripts/server-setup.ps1
+++ packer/scripts/server-setup.ps1
@@ -18,9 +18,8 @@ function Install-Choco {
         Write-Output "Chocolatey is already installed. Setting choco command."
     }
     else {
-        Set-TLS12Support
-
         Write-Output "Installing Chocolatey..."
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
         $ChocoCmd = Get-Command "choco.exe" -ErrorAction SilentlyContinue
         $ChocolateyInstall = Convert-Path "$($ChocoCmd.Path)\..\.."
         Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
