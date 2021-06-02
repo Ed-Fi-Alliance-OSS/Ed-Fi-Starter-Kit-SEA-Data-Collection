03b76072e2dc7f0661fe668966e7015024b0b1fb
semalai <m.semalaiappan@gmail.com>
2021-06-01 22:01:25 -0500

    review comments fix

--- sample-api-calls/Data Quality/ValidationError_705.sql
+++ sample-api-calls/Data Quality/ValidationError_705.sql
@@ -1,6 +1,8 @@
-USE EdFi_Ods_2022
-GO
-/****** Object:  StoredProcedure [dbo].[ValidationError_705]    Script Date: 5/11/2021 9:29:58 AM ******/
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
