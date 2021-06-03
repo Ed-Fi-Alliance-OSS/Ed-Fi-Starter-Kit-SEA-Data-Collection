83e1ab275763e85c388c8a5d1ecd1a352b351171
travis.meador <t.meador@wearedoubleline.com>
2021-06-03 14:02:15 -0500

    dynamically fill extension table with student data

--- sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/MsSql/Data/Ods/0010-SampleExtensionsData.sql
+++ sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/MsSql/Data/Ods/0010-SampleExtensionsData.sql
@@ -3,33 +3,11 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-DECLARE @grandBendHighSchoolId INT;
-DECLARE @localEducationAgencyId INT;
-DECLARE @student604822USI INT;
-DECLARE @student604823USI INT;
-DECLARE @student604822EntryDate date;
-DECLARE @student604823EntryDate date;
-
-SELECT @grandBendHighSchoolId = SchoolId , @localEducationAgencyId = localEducationAgencyId FROM edfi.School WHERE SchoolId = 255901001
-SELECT @student604822USI = StudentUSI FROM edfi.Student WHERE StudentUniqueId = '604822'
-SELECT @student604823USI = StudentUSI FROM edfi.Student WHERE StudentUniqueId = '604823'
-SELECT @student604822EntryDate = EntryDate FROM edfi.[StudentSchoolAssociation] WHERE StudentUSI = @student604822USI
-SELECT @student604823EntryDate = EntryDate FROM edfi.[StudentSchoolAssociation] WHERE StudentUSI = @student604823USI
-
-
---Sanity check to make sure some data exists, otherwise skip the script
-
-IF (@grandBendHighSchoolId IS NULL OR @student604822USI IS NULL OR @student604823USI IS NULL) RETURN
-
-INSERT INTO [sk].[StudentSchoolAssociationExtension]
-([EntryDate],[SchoolId],[StudentUSI],[ResidentLocalEducationAgencyId],[ResidentSchoolId],[ReportingSchoolId])
-VALUES
-(@student604822EntryDate,@grandBendHighSchoolId,@student604822USI,@localEducationAgencyId,@grandBendHighSchoolId,@grandBendHighSchoolId)
-
-
-INSERT INTO [sk].[StudentSchoolAssociationExtension]
-([EntryDate],[SchoolId],[StudentUSI],[ResidentLocalEducationAgencyId],[ResidentSchoolId],[ReportingSchoolId])
-VALUES
-(@student604823EntryDate,@grandBendHighSchoolId,@student604823USI,@localEducationAgencyId,@grandBendHighSchoolId,@grandBendHighSchoolId)
+INSERT INTO sk.StudentSchoolAssociationExtension (EntryDate, SchoolId, StudentUSI, ResidentLocalEducationAgencyId, ResidentSchoolId, ReportingSchoolId)
+SELECT ssa.EntryDate, ssa.EducationOrganizationId, st.StudentUSI, sc.LocalEducationAgencyId, sc.SchoolId, sc.SchoolId
+FROM edfi.Student st
+JOIN edfi.StudentSchoolAssociation ssa ON st.StudentUSI = ssa.StudentUSI
+JOIN edfi.School sc ON ssa.EducationOrganizationId = sc.SchoolId
+WHERE st.StudentUSI NOT IN (SELECT StudentUSI FROM sk.StudentSchoolAssociationExtension)
 
 GO
--- sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/PgSql/Data/Ods/0010-SampleExtensionsData.sql
+++ sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/PgSql/Data/Ods/0010-SampleExtensionsData.sql
@@ -1,28 +1,12 @@
 do $$
-declare grandBendHighSchoolId int;
-declare residentLocalEducationAgencyId int;
-declare student604822USI int;
-declare student604823USI int;
-declare student604822EntryDate date;
-declare student604823EntryDate date;
 
 begin
 
-SELECT  SchoolId ,localEducationAgencyId into grandBendHighSchoolId ,residentLocalEducationAgencyId FROM edfi.School WHERE SchoolId = 255901001;
-SELECT StudentUSI into student604822USI FROM edfi.Student WHERE StudentUniqueId = '604822';
-SELECT StudentUSI into student604823USI FROM edfi.Student WHERE StudentUniqueId = '604823';
-SELECT EntryDate into student604822EntryDate FROM edfi.StudentSchoolAssociation WHERE StudentUSI = student604822USI;
-SELECT EntryDate into student604823EntryDate FROM edfi.StudentSchoolAssociation WHERE StudentUSI = student604823USI;
-
--- Sanity check to make sure some data exists, otherwise skip the script
-if grandBendHighSchoolId is null or student604822USI is null or student604823USI is null then
-    return;
-end if;
-
-INSERT INTO sk.studentschoolassociationextension (EntryDate,SchoolId,StudentUSI,ResidentLocalEducationAgencyId,ResidentSchoolId,ReportingSchoolId)
-VALUES (student604822EntryDate,grandBendHighSchoolId,student604822USI,residentLocalEducationAgencyId,grandBendHighSchoolId,grandBendHighSchoolId);
-
-INSERT INTO sk.studentschoolassociationextension (EntryDate,SchoolId,StudentUSI,ResidentLocalEducationAgencyId,ResidentSchoolId,ReportingSchoolId)
-VALUES (student604823EntryDate,grandBendHighSchoolId,student604823USI,residentLocalEducationAgencyId,grandBendHighSchoolId,grandBendHighSchoolId);
+INSERT INTO sk.StudentSchoolAssociationExtension (EntryDate, SchoolId, StudentUSI, ResidentLocalEducationAgencyId, ResidentSchoolId, ReportingSchoolId)
+SELECT ssa.EntryDate, ssa.EducationOrganizationId, st.StudentUSI, sc.LocalEducationAgencyId, sc.SchoolId, sc.SchoolId
+FROM edfi.Student st
+JOIN edfi.StudentSchoolAssociation ssa ON st.StudentUSI = ssa.StudentUSI
+JOIN edfi.School sc ON ssa.EducationOrganizationId = sc.SchoolId
+WHERE st.StudentUSI NOT IN (SELECT StudentUSI FROM sk.StudentSchoolAssociationExtension)
 
 end $$;
