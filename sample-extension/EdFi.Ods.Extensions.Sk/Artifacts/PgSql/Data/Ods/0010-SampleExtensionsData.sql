65b652d83582e9a3a584cb7959c6fa6b24f62e33
travis.meador <t.meador@wearedoubleline.com>
2021-06-04 09:07:47 -0500

    update to use school id instead of option ed org

--- sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/PgSql/Data/Ods/0010-SampleExtensionsData.sql
+++ sample-extension/EdFi.Ods.Extensions.Sk/Artifacts/PgSql/Data/Ods/0010-SampleExtensionsData.sql
@@ -3,10 +3,10 @@ do $$
 begin
 
 INSERT INTO sk.StudentSchoolAssociationExtension (EntryDate, SchoolId, StudentUSI, ResidentLocalEducationAgencyId, ResidentSchoolId, ReportingSchoolId)
-SELECT ssa.EntryDate, ssa.EducationOrganizationId, st.StudentUSI, sc.LocalEducationAgencyId, sc.SchoolId, sc.SchoolId
+SELECT ssa.EntryDate, ssa.SchoolId, st.StudentUSI, sc.LocalEducationAgencyId, sc.SchoolId, sc.SchoolId
 FROM edfi.Student st
 JOIN edfi.StudentSchoolAssociation ssa ON st.StudentUSI = ssa.StudentUSI
-JOIN edfi.School sc ON ssa.EducationOrganizationId = sc.SchoolId
+JOIN edfi.School sc ON ssa.SchoolId = sc.SchoolId
 WHERE st.StudentUSI NOT IN (SELECT StudentUSI FROM sk.StudentSchoolAssociationExtension)
 
 end $$;
