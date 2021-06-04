3fd272679382a02f784f3bf7b85fd9abe06524a3
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2021-06-04 19:43:34 +0530

    added SQL scripts for all reports and updated packer build

--- packer/scripts/report-setup.ps1
+++ packer/scripts/report-setup.ps1
@@ -5,32 +5,12 @@
 
 Import-Module SqlServer
 
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\2000-CreateSchemas.sql"
+Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile "C:/sample-reports/2000-CreateSchemas.sql"
 
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\2010-CreateTables.sql"
+Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile "C:/sample-reports/2010-CreateTables.sql"
 
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\2050-CreateTableValueFunctions.sql"
+Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile "C:/sample-reports/2050-CreateTableValueFunctions.sql"
 
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\2100-CreateProcedureLoadStudentFallMembership.sql"
+Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile "C:/sample-reports/2100-CreateProcedureLoadStudentFallMembership.sql"
 
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\2110-CreateProcedureLoadSpedChildCount.sql"
-
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -Query "EXEC reporting.LoadStudentFallMembership" -OutputSqlErrors $true
-
-Invoke-Sqlcmd -Database "EdFi_Ods_2022" -Query "EXEC reporting.LoadSpedChildCount" -OutputSqlErrors $true
-
-New-Item -Path "C:/Users/Public/Desktop/" -Name Reports -ItemType directory
-
-if (-not (Get-InstalledModule | Where-Object -Property Name -eq "ImportExcel")) {
-    Install-Module -Force -Scope CurrentUser -Name ImportExcel
-}
-
-$SQLresults = Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile ".\Artifacts\MsSql\Structure\Ods\3100-StudentFallMembershipReport.sql" | Select * -ExcludeProperty RowError, RowState, Table, ItemArray, HasErrors
-
-$SQLresults | Export-Excel -Path "C:/Users/Public/Desktop/Reports/StudentFallMembershipReport.xlsx" -WorksheetName "Membership" -AutoSize
-
-$SQLresults = Invoke-Sqlcmd -Database "EdFi_Ods_2022" -Query "SELECT * FROM [EdFi_Ods_2022].[reporting].[SpecialEducationChildCount]" -OutputSqlErrors $true
-
-$SQLresults | Export-Excel -Path "C:/Users/Public/Desktop/Reports/SpecialEducationChildCount.xlsx" -WorksheetName "2021-2022" -AutoSize -Show
-
-
+Invoke-Sqlcmd -Database "EdFi_Ods_2022" -InputFile "C:/sample-reports/2110-CreateProcedureLoadSpedChildCount.sql"
---
+++ sample-reports/3120-SpecialEducationByAgeReport.sql
@@ -0,0 +1,23 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+
+-- =========================================================================================
+-- Query to generate SEA Starter Kit - Special Education by Age = Student Count report
+-- =========================================================================================
+SELECT [Age]
+	, [2021-2022], [2020-2021], [2019-2020], [2018-2019], [2017-2018]
+FROM  
+(
+	SELECT DISTINCT StudentUniqueId, Datayears, Age 
+	FROM [EdFi_Ods_2022].[reporting].[SpecialEducationChildCount]
+	WHERE LTRIM(RTRIM(SettingCode)) <> ''
+) AS SourceTable  
+PIVOT  
+(  
+  COUNT(StudentUniqueId)  
+  FOR Datayears IN ([2021-2022], [2020-2021], [2019-2020], [2018-2019], [2017-2018])  
+) AS PivotTable
+ORDER BY [Age]
