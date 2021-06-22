fca3d147232b1fe075cf3538017e3b8132ed0d2b
Jim McKay <james.mckay@instructure.com>
2021-06-22 11:18:35 -0500

    fixed the links after selection

--- packer/scripts/installers/swagger_index.js
+++ packer/scripts/installers/swagger_index.js
@@ -2,4 +2,5 @@
     var year = new Date().getFullYear()
     $("#schoolYearSelect option[value='" + year + "']").removeAttr("selected")
     $("#schoolYearSelect option[value='" + (year + 1) + "']").attr("selected", "selected")
+    onChangeSchoolYear()
   })
