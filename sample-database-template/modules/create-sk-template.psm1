e5c18e59d68936bcbbcd3df6733aa2d8b8e7624e
vinaya <vinaya.mayya@ed-fi.org>
2021-12-01 16:10:22 -0600

    using tpdm as start template approach with installing the extension

--- sample-database-template/modules/create-sk-template.psm1
+++ sample-database-template/modules/create-sk-template.psm1
@@ -12,7 +12,9 @@ function Get-SKConfiguration([hashtable] $config = @{ }) {
     $env:toolsPath = "$PSScriptRoot/../../../Ed-Fi-ODS-Implementation/tools/"
     $config = Merge-Hashtables (Get-DefaultTemplateConfiguration), $config
     $config.appSettings.Plugin.Folder = "$PSScriptRoot/../../../Ed-Fi-ODS-Implementation/plugin"
-    $config.appSettings.Plugin.Scripts = @("sk","tpdm")
+    $config.appSettings.Plugin.Scripts = @("sk")
+    $config.appSettings.MinimalTemplateScript = "TPDMCoreMinimalTemplate"
+    $config.appSettings.PopulatedTemplateScript = "TPDMCorePopulatedTemplate"
     $config.appSettings = Merge-Hashtables $config.appSettings, (Get-DefaultTemplateSettingsByEngine)[$config.engine]
 
     $config.testHarnessJsonConfigLEAs = @(255902, 255903)
