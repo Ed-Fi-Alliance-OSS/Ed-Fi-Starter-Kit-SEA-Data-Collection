42dd2f33180e06717a5bfc6fd368a16e43bd8451
vinaya <vinaya.mayya@ed-fi.org>
2021-12-02 12:57:15 -0600

    build update to include tpdm

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = 'Stop'
 & ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 3.1 -InstallDir 'C:/Program Files/dotnet'
 dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Folder' '../../Plugin'
 dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Folder' '../../Plugin'
-dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Scripts:0' 'sk'
-dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:0' 'sk'
+dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Scripts:1' 'sk'
+dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:1' 'sk'
 . C:/Ed-Fi-ODS-Implementation/Initialize-PowershellForDevelopment.ps1
 Initialize-DevelopmentEnvironment -RunDotnetTest -RunSdkGen -RunSmokeTest
