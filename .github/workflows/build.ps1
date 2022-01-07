dcb3dddd289a7801dbae694f06b6de1e7a58fcde
Geoff McElhanon <geoffrey@mcelhanon.com>
2022-01-07 16:21:36 -0600

    [ODS-5089] Github Action Extension build & SQL Express on Windows (#54)
    * wip
    
    * extension temp fix
    
    * [ODS-5205] Github Actions Extension Build LocalDb Failures (#53)
    
    * added edfi windows with sql container
    
    * updated container
    
    * reverted
    
    * getting localdb sql server name
    
    * getting sql server info
    
    * fixed connection string
    
    * updated connection string
    
    * getting info
    
    * rollback
    
    * getting sql info
    
    * get instance name
    
    * getting instance info
    
    * start MSSQLLocalDB
    
    * moved up MSSQLLocalDB start code
    
    * removed extra code
    
    * Restoring TLS 1.2 statement
    
    * Re-added step for configuring nuget source on .NET SDK
    
    * Add Azure Artifacts nuget feed source in lieu of redundant download and installation of .NET SDK 3.1 (#55)
    
    * Removed the step of redownloading and installing .NET SDK 3.1 (already available in windows-2019 image)
    * Add Azure Artifacts nuget feed source using dotnet.exe command-line
    
    * Modified checkout branch for Impl repo back to main
    
    * Removed obsolete user secret settings
    
    Co-authored-by: travis.meador <t.meador@wearedoubleline.com>
    Co-authored-by: raghuramakrishna <68504753+raghuramakrishna@users.noreply.github.com>

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -4,13 +4,72 @@
 # See the LICENSE and NOTICES files in the project root for more information.
 
 $ErrorActionPreference = 'Stop'
+
+$PSVersionTable
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-& ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel 3.1 -InstallDir 'C:/Program Files/dotnet'
+
+if ($env:GITHUB_ACTIONS) {
+    $basePath = $env:GITHUB_WORKSPACE
+    $version = "1.0.$env:GITHUB_RUN_NUMBER"
+    
+    # EdFi.Ods.WebApi
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'ConnectionStrings:EdFi_Ods'      'Server=(LocalDB)\\MSSQLLocalDB; Database=EdFi_{0}; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.WebApi;'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'ConnectionStrings:EdFi_Admin'    'Server=(LocalDB)\\MSSQLLocalDB; Database=EdFi_Admin; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.WebApi;'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'ConnectionStrings:EdFi_Security' 'Server=(LocalDB)\\MSSQLLocalDB; Database=EdFi_Security; Connection Timeout=30; Trusted_Connection=True; Persist Security Info=True; Application Name=EdFi.Ods.WebApi;'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'ConnectionStrings:EdFi_Master'   'Server=(LocalDB)\\MSSQLLocalDB; Database=master; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.WebApi;'
+
+    # EdFi.Ods.Api.IntegrationTestHarness
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Ods'      'Server=(LocalDB)\MSSQLLocalDB; Database=EdFi_Ods_Populated_Template_Test; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Admin'    'Server=(LocalDB)\MSSQLLocalDB; Database=EdFi_Admin; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Security' 'Server=(LocalDB)\MSSQLLocalDB; Database=EdFi_Security; Connection Timeout=30; Trusted_Connection=True; Persist Security Info=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
+    dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Master'   'Server=(LocalDB)\MSSQLLocalDB; Database=master; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
+    
+    Write-Host "Starting MSSQLLocalDB"
+    SQLLocalDB start MSSQLLocalDB
+} else {
+    $basePath = (Resolve-Path "$PSScriptRoot/../../../")
+    $version = "0.0.0"
+}
+
+# EdFi.Ods.WebApi
 dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Folder' '../../Plugin'
-dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Folder' '../../Plugin'
 dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Scripts:0' 'tpdm'
-dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:0' 'tpdm'
 dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ec 'Plugin:Scripts:1' 'sk'
-dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:1' 'sk'
-. C:/Ed-Fi-ODS-Implementation/Initialize-PowershellForDevelopment.ps1
+
+# EdFi.Ods.Api.IntegrationTestHarness
+dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Folder' '../../Plugin'
+dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:0' 'tpdm'
+dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'Plugin:Scripts:1' 'sk'   
+
+dotnet nuget add source $env:AZURE_ARTIFACTS_FEED_URL --name EdFiAzureArtifacts
+
+(Get-ChildItem $basePath).FullName
+. $basePath/Ed-Fi-ODS-Implementation/Initialize-PowershellForDevelopment.ps1
+
+$skExtensionPath = "$basePath/Starter-Kit-SEA-Modernization/sample-extension/EdFi.Ods.Extensions.Sk/"
+Invoke-CodeGen -Engine SQLServer -ExtensionPaths $skExtensionPath
+& dotnet build $skExtensionPath --configuration release
+& dotnet test $skExtensionPath --no-restore --verbosity normal
+
+$nuget = Install-NuGetCli (Get-ToolsPath)
+& $nuget 
+$packagesPath = "$basePath/Starter-Kit-SEA-Modernization/.github/workflows/packages/"
+& $nuget pack $skExtensionPath/EdFi.Ods.Extensions.Sk.nuspec `
+    -OutputDirectory $packagesPath `
+    -Version $version `
+    -Properties configuration=release `
+    -Properties "authors=Ed-Fi Alliance" `
+    -Properties "owners=Ed-Fi Alliance" `
+    -Properties "copyright=Copyright ©Ed-Fi Alliance, LLC. 2020" `
+    -Properties id=EdFi.Ods.Extensions.Sk `
+    -Properties title=EdFi.Ods.Extensions.Sk `
+    -Properties description=EdFi.Ods.Extensions.Sk `
+    -NoPackageAnalysis `
+    -NoDefaultExcludes
+Copy-Item $packagesPath/EdFi.Ods.Extensions.Sk.$version.nupkg $packagesPath/EdFi.Ods.Extensions.Sk.zip
+Expand-Archive $packagesPath/EdFi.Ods.Extensions.Sk.zip $basePath/Ed-Fi-ODS-Implementation/Plugin/EdFi.Ods.Extensions.Sk/ -Force
+
 Initialize-DevelopmentEnvironment -RunDotnetTest -RunSdkGen -RunSmokeTest
+
+# & dotnet nuget push $packagesPath/EdFi.Ods.Extensions.Sk.$version.nupkg --api-key AzureArtifacts --skip-duplicate
