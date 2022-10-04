811402ebe8f1ab8a17bb629f47e88c44f264fd82
semalai <m.semalaiappan@gmail.com>
2022-10-04 12:31:56 -0500

    nuget pack updated

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -24,7 +24,7 @@ if ($env:GITHUB_ACTIONS) {
     dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Admin'    'Server=(LocalDB)\MSSQLLocalDB; Database=EdFi_Admin; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
     dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Security' 'Server=(LocalDB)\MSSQLLocalDB; Database=EdFi_Security; Connection Timeout=30; Trusted_Connection=True; Persist Security Info=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
     dotnet user-secrets set --id f1506d66-289c-44cb-a2e2-80411cc690ed 'ConnectionStrings:EdFi_Master'   'Server=(LocalDB)\MSSQLLocalDB; Database=master; Connection Timeout=30; Trusted_Connection=True; Application Name=EdFi.Ods.Api.IntegrationTestHarness'
-    
+
     Write-Host "Starting MSSQLLocalDB"
     SQLLocalDB start MSSQLLocalDB
 } else {
@@ -53,20 +53,23 @@ Invoke-CodeGen -Engine SQLServer -ExtensionPaths $skExtensionPath
 & dotnet test $skExtensionPath --no-restore --verbosity normal
 
 $nuget = Install-NuGetCli (Get-ToolsPath)
-& $nuget 
-$packagesPath = "$PSScriptRoot\NugetPackages"
+& $nuget
+$packagesPath = "$basePath/Starter-Kit-SEA-Modernization/.github/workflows/packages/"
 & $nuget pack $skExtensionPath/EdFi.Ods.Extensions.Sk.nuspec `
     -OutputDirectory $packagesPath `
     -Version $version `
     -Properties configuration=$Configuration `
-    -Properties id=EdFi.Suite3.Ods.Extensions.Sk `
+    -Properties "authors=Ed-Fi Alliance" `
+    -Properties "owners=Ed-Fi Alliance" `
+    -Properties "copyright=Copyright ©Ed-Fi Alliance, LLC. 2020"
+    -Properties id=EdFi.Ods.Extensions.Sk `
     -Properties title=EdFi.Ods.Extensions.Sk `
     -Properties description=EdFi.Ods.Extensions.Sk `
     -NoPackageAnalysis `
     -NoDefaultExcludes
-Copy-Item $packagesPath/EdFi.Suite3.Ods.Extensions.Sk.$version.nupkg $packagesPath/EdFi.Ods.Extensions.Sk.zip
+Copy-Item $packagesPath/EdFi.Ods.Extensions.Sk.$version.nupkg $packagesPath/EdFi.Ods.Extensions.Sk.zip
 Expand-Archive $packagesPath/EdFi.Ods.Extensions.Sk.zip $basePath/Ed-Fi-ODS-Implementation/Plugin/EdFi.Ods.Extensions.Sk/ -Force
 
 Initialize-DevelopmentEnvironment -RunDotnetTest -RunSdkGen -RunSmokeTest
 
-# & dotnet nuget push $packagesPath/EdFi.Ods.Extensions.Sk.$version.nupkg --api-key AzureArtifacts --skip-duplicate
+& dotnet nuget push $packagesPath/EdFi.Ods.Extensions.Sk.$version.nupkg --api-key AzureArtifacts --skip-duplicate
